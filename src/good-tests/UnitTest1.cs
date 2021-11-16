62bd61b539baaf999ab5fe1749bc672f9d1b1048
Erwin van der Valk <erwin@vandervalk.pro>
2021-11-16 16:09:53 +0100

    written about good tests

---
+++ src/good-tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Xunit;
+
+namespace good_tests
+{
+  public class RestaurantTests
+  {
+    [Fact]
+    public void Given_no_chef_in_restaurant_When_cooking_then_exception()
+    {
+
+    }
+
+  }
+
+  public class TestData
+  { }
+
+
+  public class TestDataBuilder
+  {
+
+  }
+
+  public class Restaurant
+  {
+    public Restaurant()
+    {
+
+    }
+  }
+
+
+
+}
